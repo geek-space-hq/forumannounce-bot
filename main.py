dbac24f3a26a0c7d7902eb5184ac377f1fad5cd3
bacon <bacon0817.ix@gmail.com>
2023-11-08 10:53:34 +0900

    フォーラムに新規投稿があった場合に特定のチャンネルに通知をする

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 
 BOT_TOKEN=os.environ['forumannouce-bot-API-TOKEN']
 SETTING_JSON="settings.json"
+SEND_CHANNEL_ID='send_messagechannel_ID'
 intents = discord.Intents.default()
 intents.members = True # メンバー管理の権限
 intents.message_content = True # メッセージの内容を取得する権限
@@ -19,17 +20,25 @@ async def on_ready():
     print("login!")
     await tree.sync()#スラッシュコマンドを同期
 
+@bot.event
 async def on_thread_create(thread: discord.Thread):
-    if(thread.parent.type=="forum"): #forumの新規ポストを監視する
-        print("forum create!!")
+    print("new tread!\n tread type:"+str(thread.parent.type))
+    if(str(thread.parent.type)=="forum"): #forumの新規ポストを監視する
+        print("new post!")
+        with open(SETTING_JSON,'r')as f:#設定用json読み込み
+            settings=json.load(f)
+        send_channel=bot.get_channel(settings[SEND_CHANNEL_ID])
+        await send_channel.send("フォーラムにポストが追加されました\n"+thread.name)
+        thread.name
 
 @tree.command(name="frgs",description="フォーラム通知を流すチャンネルをこのチャンネルに設定します")
 async def test_command(interaction: discord.Interaction):
-    with open(SETTING_JSON,'r')as f:
+    
+    with open(SETTING_JSON,'r')as f:#設定用json読み込み
         settings=json.load(f)
-    print(settings)
-    settings['send_messagechannel_ID']=interaction.channel_id
+        
+    settings['send_messagechannel_ID']=interaction.channel_id #コマンドが呼び出されたチャンネルIDを保存
     with open(SETTING_JSON,'w')as f:
         json.dump(settings,f)
-    await interaction.response.send_message(interaction.channel_id,ephemeral=True)
+    await interaction.response.send_message('通知チャンネル設定しました ID:'+str(interaction.channel_id),ephemeral=False)
 bot.run(BOT_TOKEN)
